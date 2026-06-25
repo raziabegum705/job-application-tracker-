a0122add68954e149b40e43e5995320231f57a88
Razia <raziabegum37567@gmail.com>
2026-06-25 13:49:49 +0530

    api

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,20 @@
-import axios from "axios";
 
+import axios from "axios";
+ 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
-
+ 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  const stored = localStorage.getItem("jobTrackerUser");
+  if (stored) {
+    const parsed = JSON.parse(stored);
+    if (parsed?.token) {
+      config.headers.Authorization = `Bearer ${parsed.token}`;
+    }
+  }
   return config;
 });
-
+ 
 export default api;
+ 
